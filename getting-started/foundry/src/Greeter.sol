2b2111ab5499f2314794043a9e5d289066b14739
dembelekaro <157847339+dembelekaro@users.noreply.github.com>
2024-01-26 21:02:01 +0000

    Update Greeter.sol

--- getting-started/foundry/src/Greeter.sol
+++ getting-started/foundry/src/Greeter.sol
@@ -1,4 +1,39 @@
 //SPDX-License-Identifier: Unlicense
+
+
+
+
+contract Greeter {
+  string greeting;
+
+  constructor(string memory _greeting) {
+//    console.log("Deploying a Greeter with greeting:", _greeting);
+    greeting = _greeting;
+  }
+
+  function greet() public view returns (string memory) {
+    return greeting;
+  }
+
+  function setGreeting(string memory _greeting) public {
+//    console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+    greeting = _greeting;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 // import "hardhat/console.sol";
